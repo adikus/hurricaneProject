1c00bfbc37007f0c99214b510e5334f10d28bbb6
adikus <andrej.hoos@gmail.com>
2016-03-01 14:25:53 +0000

    more spark scripts

---
+++ LES/scripts/start_cpu_worker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/conf.cfg
+
+export MGRID_PATH=$MGRID_FILE_PATH
+export APARAPI_CL_BIN_FOLDER=$DIR/../cl
+export SPARK_WORKER_DIR=$WORK_DIR
+export LES_LIB_PATH=$DIR/../src/main/resources/libles_ocl.so
+export SPARK_JAVA_OPTS="-Djava.library.path=$APARAPI_JNI_PATH -Dcom.amd.aparapi.flowType=binary -Djna.nosys=true -Dcom.amd.aparapi.executionMode=CPU" 
+$SPARK_PATH/bin/spark-class org.apache.spark.deploy.worker.Worker -m 4G --cores 1 spark://$IP:7077
